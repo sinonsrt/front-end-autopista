f161ba24eb41d542dc22457d3bfa46c559e80c7f
Lucas Marchori <sinonsrt@hotmail.com>
2021-05-19 20:08:39 -0300

    Criado estrutura das páginas

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { Button } from "@material-ui/core";
+import { Button, FormControl, InputLabel, Input, FormHelperText} from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles({
   root: {
-    backgroundColor: "gray",
-    height: '100vh',
+    backgroundColor: "white",
+    height: "100vh",
   },
 });
 
@@ -13,7 +13,14 @@ const Login: React.FC = () => {
   const classes = useStyles();
   return (
     <div className={classes.root}>
-      <Button variant="contained" color="primary">
+      <FormControl>
+        <InputLabel htmlFor="my-input">Email address</InputLabel>
+        <Input id="my-input" aria-describedby="my-helper-text" />
+        <FormHelperText id="my-helper-text">
+          We'll never share your email.
+        </FormHelperText>
+      </FormControl>
+      <Button variant=  "contained" color="primary">
         Secondary
       </Button>
     </div>
