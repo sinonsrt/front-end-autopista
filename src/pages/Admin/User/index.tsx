df95223db49cd3a0186a283628d246b7bf70985c
Lucas Marchiori <sinonsrt@hotmail.com>
2021-05-21 20:24:53 -0300

    Criado básico de cada formulário

--- src/pages/Admin/User/index.tsx
+++ src/pages/Admin/User/index.tsx
@@ -1,9 +1,136 @@
-import React from "react"
+import React from "react";
+import { Formik, Form } from "formik";
+import {
+  Grid,
+  Paper,
+  Typography,
+  useMediaQuery,
+  Button,
+  FilledInput,
+} from "@material-ui/core";
+import TextInput from "../../../components/TextInput";
+import Select from "../../../components/Select";
+import {
+  makeStyles,
+  createStyles,
+  Theme,
+  useTheme,
+} from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    paper: {
+      borderBottomRightRadius: 0,
+      borderBottomLeftRadius: 0,
+      flexDirection: "column",
+      padding: theme.spacing(4),
+      paddingRight: theme.spacing(3),
+
+      [theme.breakpoints.down("xs")]: {
+        paddingBottom: 130,
+      },
+    },
+    paperBotton: {
+      borderTopRightRadius: 0,
+      borderTopLeftRadius: 0,
+      padding: theme.spacing(1),
+      background: "#f9f9f9",
+    },
+    buttonSave: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.success.main,
+      boxShadow: "none",
+      color: "#fff",
+      "&:hover": {
+        backgroundColor: "#208c4e",
+      },
+    },
+  })
+);
 
 const User: React.FC = () => {
-    return (
-        <h1>User</h1>
-    )
-}
+  const classes = useStyles();
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.down("xs"));
+
+  return (
+    <div>
+      <Formik
+        initialValues={{ description: "" }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {({ values, setFieldValue }) => (
+          <Form>
+            <Paper className={classes.paper}>
+              <Typography variant="h3">Cadastro</Typography>
+              <Typography variant="h5">Usuário</Typography>
+
+              <Grid item xs={12} sm={8} md={12}>
+                <TextInput name="name" label="Nome" />
+              </Grid>
+              <Grid item xs={12} sm={8} md={12}>
+                <TextInput name="email" label="E-mail" />
+              </Grid>
+              <Grid item xs={12} sm={8} md={12}>
+                <TextInput name="password" label="Senha" />
+              </Grid>
+              <Grid item xs={12} sm={8} md={12}>
+                <TextInput name="phone" label="Telefone" />
+              </Grid>
+              <Grid item xs={12} sm={8} md={12}>
+                <TextInput name="avatar" label="avatar" />
+              </Grid>
+              <Grid item xs={12} sm={8} md={12}>
+                <Select
+                  name="city"
+                  label="Cidade"
+                  options={[
+                    { id: "Umuarama", text: "Umuarama" },
+                    { id: "Maringá", text: "Maringá" },
+                  ]}
+                />
+              </Grid>
+              <Grid item xs={12} sm={8} md={12}>
+                <Select
+                  name="access_level"
+                  label="Nível de Acesso"
+                  options={[
+                    { id: "01", text: "Administrador" },
+                    { id: "02", text: "Usuário" },
+                    { id: "02", text: "Empresarial" },
+                  ]}
+                />
+              </Grid>
+            </Paper>
+
+            <Paper className={classes.paperBotton}>
+              <Grid
+                container
+                spacing={1}
+                direction="row"
+                justify="flex-start"
+                alignItems={matches ? "flex-start" : "center"}
+              >
+                <Grid item xs={12} sm={12} md={4}>
+                  <Button
+                    type="submit"
+                    disableElevation
+                    variant="contained"
+                    style={{ float: "right" }}
+                    className={classes.buttonSave}
+                  >
+                    Gravar
+                  </Button>
+                </Grid>
+              </Grid>
+            </Paper>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
 
 export default User;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,15 +7,31 @@ import Register from '../pages/Register';
 import ExampleForm from '../pages/ExampleForm';
 
 import AccessLevel from '../pages/Admin/AccessLevel'
+import Code from '../pages/Admin/Code'
 import Company from '../pages/Admin/Company'
+import Dashboard from '../pages/Admin/Dashboard'
+import GasService from '../pages/Admin/GasService'
+import News from '../pages/Admin/News'
+import Service from '../pages/Admin/Service'
+import Types from '../pages/Admin/Types'
+import User from '../pages/Admin/User'
 
 const Routes: React.FC = () => (
     <Switch>
         <Route path="/" exact component={Login} title="Login" isPrivate />
-        <Route path="/AccessLevel" exact component={AccessLevel} title="AccessLevel" isPrivate />
-        <Route path="/Company" exact component={Company} title="Company" isPrivate />
         <Route path="/register" exact component={Register} title="Register" isPrivate />
-        <Route path="/ExampleForm" exact component={ExampleForm} title="ExampleForm" />
+
+        <Route path="/exampleForm" exact component={ExampleForm} title="ExampleForm" />
+
+        <Route path="/accessLevel" exact component={AccessLevel} title="AccessLevel" isPrivate />
+        <Route path="/code" exact component={Code} title="Code" isPrivate />
+        <Route path="/company" exact component={Company} title="Company" isPrivate />
+        <Route path="/dashboard" exact component={Dashboard} title="Dashboard" isPrivate />
+        <Route path="/gasService" exact component={GasService} title="GasService" isPrivate />
+        <Route path="/news" exact component={News} title="News" isPrivate />
+        <Route path="/service" exact component={Service} title="Service" isPrivate />
+        <Route path="/types" exact component={Types} title="Types" isPrivate />
+        <Route path="/user" exact component={User} title="User" isPrivate />
 
         {/* <Route component={NotFound} isPrivate title="NotFound" /> */}
     </Switch>
